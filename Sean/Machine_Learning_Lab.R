d1046ab46d485f18d4e083b313df9295350198f0
longnym <longnym@gmail.com>
2019-11-05 00:33:22 -0500

    R code update

--- Sean/Machine_Learning_Lab.R
+++ Sean/Machine_Learning_Lab.R
@@ -33,17 +33,17 @@ orders$Order.ID = as.character(orders$Order.ID)
 returns$Order.ID = as.character(returns$Order.ID)
 
 orders_returns <- left_join(orders, returns, by='Order.ID')
-orders_returns$Returned = as.character(orders_returns$Returned)
-orders_returns$Returned[is.na(orders_returns$Returned)] <- 'No'
+orders_returns$Returned = as.numeric(orders_returns$Returned)
+orders_returns$Returned[is.na(orders_returns$Returned)] <- 0
 
 # 1. How much profit did we lose due to returns each year?
-lose_profit <- orders_returns[orders_returns$Returned == 'Yes',] %>% group_by(year=format(Order.Date, '%Y')) %>% summarise(total_lost_profit=sum(Profit))
+lose_profit <- orders_returns[orders_returns$Returned == 1,] %>% group_by(year=format(Order.Date, '%Y')) %>% summarise(total_lost_profit=sum(Profit))
 ggplot(data = lose_profit, aes(x=year, y=total_lost_profit, group=1)) +
   geom_line() +
   geom_point()
 
 # 2. How many customer returned more than once? more than 5 times?
-customer_returned <- orders_returns[orders_returns$Returned == 'Yes',] %>% group_by(custimer=Customer.ID) %>% summarise(total_returned=n())
+customer_returned <- orders_returns[orders_returns$Returned == 1,] %>% group_by(custimer=Customer.ID) %>% summarise(total_returned=n())
 nrow(customer_returned) # 1061
 nrow(customer_returned[customer_returned$total_returned > 5,]) # 46
 
@@ -54,7 +54,41 @@ ggplot(data = regeon_returned, aes(x=reorder(region, total_returned), y=total_re
   coord_flip()
 
 # 4. Which categories (sub-categories) of products are more likely to be returned?
-category_returned <- orders_returns[orders_returns$Returned == 'Yes',] %>% group_by(category=Sub.Category) %>% summarise(total_returned=n()) %>% arrange(desc(total_returned))
+category_returned <- orders_returns[orders_returns$Returned == 1,] %>% group_by(category=Sub.Category) %>% summarise(total_returned=n()) %>% arrange(desc(total_returned))
 ggplot(data = category_returned, aes(x=reorder(category, total_returned), y=total_returned)) +
   geom_bar(stat='identity') +
   coord_flip()
+
+# Problem 4: Feature Engineering
+orders_returns$Process.Time <- as.numeric(orders_returns$Ship.Date - orders_returns$Order.Date)
+
+returned_before <- orders_returns[orders_returns$Returned == 1,] %>% group_by(Product.ID) %>% summarise(Returned.Before=n())
+orders_returns <- left_join(orders_returns, returned_before, by='Product.ID')
+orders_returns$Returned.Before[is.na(orders_returns$Returned.Before)] <- 0
+
+# Problem 5: Fitting Models
+library(caret)
+
+set.seed(0)
+index <- createDataPartition(orders_returns$Returned, p=0.8, list=FALSE)
+train <- orders_returns[index,]
+test <- orders_returns[-index,]
+
+model <- glm(Returned ~ Process.Time + Returned.Before + Shipping.Cost + Sales,
+                    family = 'binomial',
+                    data = train)
+summary(model)
+
+# Problem 6: Evaluating Models
+pred.prob <- predict(model, newdata=test, type='response')
+pred.class = ifelse(pred.prob >= 0.5, 1, 0)
+
+accuracy <- mean((pred.prob >= 0.5) == (test$Returned == 1))
+
+confusionMatrix(factor(pred.class, levels=c(0,1)), factor(test$Returned, levels=c(0,1)), positive='1')
+
+library(pROC)
+
+modelroc = roc(test$Returned, pred.class)
+plot(modelroc, print.auc=TRUE, auc.polygon=TRUE, grid=c(0.1, 0.2), grid.col=c("green", "red"),
+     max.auc.polygon=TRUE, auc.polygon.col="skyblue", print.thres=TRUE)
