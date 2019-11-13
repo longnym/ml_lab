cfdd421a489a190d47c726b1e55041c8b0635673
longnym <longnym@gmail.com>
2019-11-13 16:13:23 -0500

    update XGBoost

--- Sean/ML_Project_XGB.R
+++ Sean/ML_Project_XGB.R
@@ -3,17 +3,17 @@ library(xgboost)
 validation_new$Predict = NULL
 
 dtrain <- xgb.DMatrix(data=as.matrix(train_new %>% select(-SalePrice)), label=train_new$SalePrice, missing=NA)
-feature_names <- names(train_new %>% select(-SalePrice))
 dtest <- xgb.DMatrix(data=as.matrix(validation_new %>% select(-SalePrice)), missing=NA)
 
-xgb.mdl <- xgb.train(data = dtrain, nround=100)
+xgb.mdl <- xgb.train(data = dtrain, nround=1000)
 validation_new$Predict = predict(xgb.mdl, dtest)
 
 # Compute feature importance matrix
+feature_names <- names(train_new %>% select(-SalePrice))
 importance_matrix <- xgb.importance(feature_names, model = xgb.mdl)
 
 # Nice graph
-xgb.plot.importance(importance_matrix[1:10,])
+xgb.plot.importance(importance_matrix[1:20,])
 
 # RMSE
 sqrt(sum((validation_new$Predict - validation_new$SalePrice) ^ 2) / nrow(validation_new))
