161cfb71b076ba84e24fb343d51e5cb26cd3f2ea
longnym <longnym@gmail.com>
2019-11-13 19:55:05 -0500

    update R code

--- Sean/ML_Project_XGB.R
+++ Sean/ML_Project_XGB.R
@@ -3,7 +3,16 @@ library(xgboost)
 dtrain <- xgb.DMatrix(data=as.matrix(train_new %>% select(-log.SalePrice)), label=train_new$log.SalePrice, missing=NA)
 dtest <- xgb.DMatrix(data=as.matrix(validation_new %>% select(-log.SalePrice)), missing=NA)
 
-xgb.mdl <- xgb.train(data = dtrain, nround=1000)
+foldsCV <- createFolds(train_new$log.SalePrice, k=7, list=TRUE, returnTrain=FALSE)
+
+param <- list(booster = "gblinear", objective = "reg:linear", eval_metric = 'rmse')
+
+xgb_cv <- xgb.cv(data = dtrain, params = param, nrounds = 1000, prediction = TRUE, maximize = FALSE,
+                 folds = foldsCV, print_every_n = 20)
+
+nrounds <- which(xgb_cv$pred == min(xgb_cv$pred))
+
+xgb.mdl <- xgb.train(data = dtrain, nround=nrounds)
 pred = predict(xgb.mdl, dtest)
 
 # Compute feature importance matrix
